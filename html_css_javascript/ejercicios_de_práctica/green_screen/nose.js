b8b276ee19e1abe607ba4b6cd8b2f4116e0bfe24
silvana804 <sisuarezc@unal.edu.co>
2023-06-23 18:42:42 -0500

    hola

---
+++ html_css_javascript/ejercicios_de_práctica/green_screen/nose.js
@@ -0,0 +1,8 @@
+// Objeto
+var fgImage = new SimpleImage("drewRobert.png");
+var dImage=new SimpleImage("drewRobert.png");
+// Método
+var w= fgImage.getWidth();
+var w2= dImage.getWidth();
+var h= fgImage.getHeight();
+var h2= dImage.getWidth();
---
+++ html_css_javascript/javascript/básico/datos_primitivos/null_undefined_NaN.js
@@ -0,0 +1,6 @@
+var indefinida; //no se le ha asignado un valor
+
+var nulo = null; // el programador le asignó el valor ausente
+
+//NaN not a number
+var noUnNumero = "hola" * 3.7;
