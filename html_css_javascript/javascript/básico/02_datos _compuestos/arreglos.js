b9bb7a812bfd4e89b2e7b81799d2765cd69d7625
silvana804 <sisuarezc@unal.edu.co>
2023-06-24 18:21:43 -0500

    hola

---
+++ html_css_javascript/javascript/básico/02_datos _compuestos/arreglos.js
@@ -0,0 +1,16 @@
+const arreglito = [];
+
+const c = Array.of("x", "y");
+
+const d = Array(100).fill(false); // iincializar múltiples posiciones con un valor
+
+const colores = ["rojo", "verde", "azul"];
+colores.push("NegRO"); //agrega un color al final
+
+colores.pop(); // elimina el último elemento de una función
+
+colores.forEach(function (el, index) {
+  console.log(<li id="${index}">${el}</li>); // ejecuta una función para cada elemento del arreglo
+});
+
+// ${valor} no solo es para comillas
